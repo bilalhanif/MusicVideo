b739ab78ca66f375a1b6d00cef369575e750b047
Logic Networks <administrator@alis-macbook.local>
2016-04-28 15:44:57 +0500

    Part 4 completed

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by Bilal on 4/27/16.
+//  Copyright © 2016 LNL. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+
+typealias JSONArray = Array<AnyObject>
+
