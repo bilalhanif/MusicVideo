e8a3853ec4506759a9f140e34c60e72aff1d7d42
Logic Networks <administrator@alis-macbook.local>
2016-04-28 18:06:26 +0500

    add additional fields to our class from our json data (part 5)

--- MusicVideo/MusicVideo.swift
+++ MusicVideo/MusicVideo.swift
@@ -13,8 +13,18 @@ class Videos {
     //Data Encapsulation
     
     private var _vName:String
+    private var _vRights:String
+    private var _vPrice:String
     private var _vImageUrl:String
+    private var _vArtist:String
     private var _vVideoUrl:String
+    private var _vImid:String
+    private var _vGenre:String
+    private var _vLinkToitTunes:String
+    private var _vReleaseDte:String
+    
+    //this variable gets created from the UI
+    var vImageData:NSData?
     
     //Make a getter
     
@@ -22,12 +32,39 @@ class Videos {
         return _vName
     }
     
+    var vRights: String {
+        return _vRights
+    }
+    var vPrice: String {
+        return _vPrice
+    }
+    
     var vImageUrl: String {
         return _vImageUrl
     }
     
+    var vArtist: String {
+        return _vArtist
+    }
+    
     var vVideoUrl: String {
         return _vVideoUrl
+    }
+    
+    var vImid: String {
+        return _vImid
+    }
+    
+    var vGenre: String {
+        return _vGenre
+    }
+    
+    var vLinkToitTunes: String {
+        return _vLinkToitTunes
+    }
+    
+    var vReleaseDte: String {
+        return _vReleaseDte
     }
     
     init(data: JSONDictionary) {
@@ -36,7 +73,6 @@ class Videos {
         //return from initializer without initializing all stored properties
         
         //video name
-        
         if let name = data["im:name"] as? JSONDictionary,
             vName = name["label"] as? String {
                 self._vName = vName
@@ -46,6 +82,24 @@ class Videos {
             //element in the JSON is unexpected
             
             _vName = ""
+        }
+        
+        //the studio name
+        if let rights = data["rights"] as? JSONDictionary,
+            vRights = rights["label"] as? String {
+                self._vRights = vRights
+        }
+        else {
+            _vRights = ""
+        }
+        
+        //price of video
+        if let price = data["img:price"] as? JSONDictionary,
+            vPrice = price["label"] as? String {
+            self._vPrice = vPrice
+        }
+        else {
+            _vPrice = ""
         }
         
         //the video image
@@ -58,6 +112,15 @@ class Videos {
             _vImageUrl = ""
         }
         
+        //the artist name
+        if let artist = data["im:artist"] as? JSONDictionary,
+            vArtist = artist["label"] as? String {
+                self._vArtist = vArtist
+        }
+        else {
+            _vArtist = ""
+        }
+        
         //video url
         if let video = data["link"] as? JSONArray,
             vUrl = video[1] as? JSONDictionary,
@@ -67,6 +130,45 @@ class Videos {
         }
         else {
             _vVideoUrl = ""
+        }
+        
+        //the artist ID for iTunes search API
+        if let imid = data["id"] as? JSONDictionary,
+            vid = imid["attributes"] as? JSONDictionary,
+            vImid = vid["im:id"] as? String {
+                self._vImid = vImid
+        }
+        else {
+            _vImid = ""
+        }
+        
+        //the genre
+        if let genre = data["category"] as? JSONDictionary,
+            rel2 = genre["attributes"] as? JSONDictionary,
+            vGenre = rel2["term"] as? String {
+            self._vGenre = vGenre
+        }
+        else {
+            _vGenre = ""
+        }
+
+        //video link to iTunes
+        if let release2 = data["id"] as? JSONDictionary,
+            vLinkToitTunes = release2["label"] as? String {
+                self._vLinkToitTunes = vLinkToitTunes
+        }
+        else {
+            _vLinkToitTunes = ""
+        }
+        
+        //the release date
+        if let release2 = data["im:releaseDate"] as? JSONDictionary,
+            rel2 = release2["attributes"] as? JSONDictionary,
+            vReleaseDte = rel2["label"] as? String {
+                self._vReleaseDte = vReleaseDte
+        }
+        else {
+            _vReleaseDte = ""
         }
     }
     
